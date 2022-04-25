085379125a65f1c42c60b5d1cbeb83dc78ae8871
LENOVO <hegdenagashree19@gmail.com>
2022-04-25 22:56:32 +0530

    a

--- blog/urls.py
+++ blog/urls.py
@@ -19,4 +19,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app_blog.urls')),
+    path('members/',include('django.contrib.auth.urls')),
+    path('',include('members.urls')),
+
 ]
