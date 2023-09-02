cd20853494f119bab86e3c846e51564efebdb062
unknown <hegdenagashree19@gmail.com>
2023-09-02 21:46:33 +0530

    added the forms

--- members/views.py
+++ members/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import PasswordChangeForm,UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
-from .forms import SignUpForm, EditProfileForm
+from django.contrib.auth.views import PasswordChangeView
+from .forms import PasswordChangingForm, SignUpForm, EditProfileForm
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    #form_class = PasswordChangeForm
+    success_url = reverse_lazy('password_success')
+
+def password_success(request):
+   return render(request, 'registration/password_success.html', {})
+
+
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
